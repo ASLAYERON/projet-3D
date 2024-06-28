c7816c9e010305f3755aac8f9be74c68be29778f
ASLAYERON <albert.boivin54@gmail.com>
2024-06-28 17:10:49 +0200

    texture v3 et protype hitbox (beugué)

--- main.lua
+++ main.lua
@@ -3,7 +3,9 @@ love.window.setMode(SCREEN_W,SCREEN_H)
 texture1 = love.graphics.newImage("texture1.png")
 texture2 = love.graphics.newImage("texture2.png")
 texture3 = love.graphics.newImage("texture3.png")
+texture4 = love.graphics.newImage("texture4.png")
 BACKGROUND = love.graphics.newImage("BACKGROUND.png")
+UI = love.graphics.newImage("ui.png")
 
 local function abs(x)
     return x/400-1
@@ -17,37 +19,39 @@ end
 local function ySCREEN(ord)
     return math.floor((ord+1)*225+0.5)
 end
-local xPlayer = 0
-local yPlayer = 0
+local xPlayer = 5
+local yPlayer = 5
+local newxPlayer = 0
+local newyPlayer = 0
 local alpha = 0
 local h={}
-local tex={}
+local tex={} 
 local texGlobal={}
 local texture={}
 
 L=20
 W=20
 level = {
-    "11232132332321113232",
-    "1....3.....2.......1",
-    "2....2.............2",
-    "3....2.....112332111",
+    "11121411311241131143",
+    "1....1.....2.......1",
+    "1....4.............2",
+    "3....1.....112312141",
     "2....3.............2",
-    "3...............2..2",
+    "4.........1.....2..2",
     "1...............3..3",
-    "13221233123123232..1",
-    "1............2.....1",
+    "1112113114311141...1",
+    "1............2.....4",
     "2............3...111",
     "3....1.......1.....1",
     "1....1.......3.....1",
-    "3..3233123..1231...1",
-    "3..3.........3.....1",
-    "3..3.........1..1331",
-    "1333...2131312..2..1",
-    "1............3..2..3",
-    "2..................1",
-    "1............3.....2",
-    "11313212313313223332",
+    "4..4124131..1141...4",
+    "4..4.........3.....1",
+    "3..3.........1..1231",
+    "1331...1121131..1..1",
+    "1............1..2..3",
+    "4..................1",
+    "1............1.....4",
+    "11113142113112113131",
 
 }
 
@@ -55,7 +59,11 @@ level = {
 
 
 function love.update(dt)
-    print(string.byte(yPlayer,xPlayer))
+    --print (string.byte(level[math.floor((yPlayer-2)/2+0.5)],math.floor((xPlayer-2)/2+0.5))..string.byte(level[math.floor((yPlayer-2)/2+0.5+1)],math.floor((xPlayer-2)/2+0.5))..string.byte(level[math.floor((yPlayer-2)/2+0.5+1)],math.floor((xPlayer-2)/2+0.5+1)))
+    --print(string.byte(level[math.floor((yPlayer-2)/2+0.5)],math.floor((xPlayer-2)/2+0.5-1))..".."..string.byte(level[math.floor((yPlayer-2)/2+0.5)],math.floor((xPlayer-2)/2+0.5+1)))
+    --print (string.byte(level[math.floor((yPlayer-2)/2+0.5-1)],math.floor((xPlayer-2)/2+0.5-1))..string.byte(level[math.floor((yPlayer-2)/2+0.5-1)],math.floor((xPlayer-2)/2+0.5))..string.byte(level[math.floor((yPlayer-2)/2+0.5-1)],math.floor((xPlayer-2)/2+0.5+1)))
+    --print("......")
+    print(alpha)
     if love.keyboard.isDown( "right" ) then
         alpha  = alpha -.02
     end
@@ -63,14 +71,19 @@ function love.update(dt)
     if love.keyboard.isDown( "left" ) then
         alpha  = alpha +.02
     end
-    
     if love.keyboard.isDown( "up" ) then
-            xPlayer,yPlayer  = xPlayer -.2*math.sin(alpha), yPlayer +.2*math.cos(alpha) 
+            newxPlayer  = xPlayer -.2*math.sin(alpha)
+            newyPlayer  = yPlayer +.2*math.cos(alpha)
+            if string.byte(level[math.floor((newyPlayer)/2+0.5)],math.floor((newxPlayer)/2+0.5))==46 then
+                xPlayer,yPlayer=newxPlayer,newyPlayer
+            end
 	end
 
     if love.keyboard.isDown( "down" ) then
-       xPlayer,yPlayer  = xPlayer +.2*math.sin(alpha), yPlayer -.2*math.cos(alpha) 
+       xPlayer,yPlayer  = xPlayer +.2*math.sin(alpha), yPlayer -.2*math.cos(alpha)
 	end
+
+    if alpha >=6 or alpha <=-6 then alpha=0 end
 end
 function  in_avatar_coord(x,y)
     x,y = x-xPlayer,y-yPlayer
@@ -80,7 +93,7 @@ function  in_avatar_coord(x,y)
 end
 
 function draw_segment(x1,y1,u1,x2,y2,u2,T)
-    if y1<=.001 and  y2<=.001 then return end
+    if y1<=0.001 and  y2<=0.001 then return end
     local X1 = x1/y1
     local X2 =x2/y2
     if X1>X2 then
@@ -94,7 +107,6 @@ function draw_segment(x1,y1,u1,x2,y2,u2,T)
     end
     local H1 = (SCREEN_W/SCREEN_H)/y1
     local H2 = (SCREEN_W/SCREEN_H)/y2
-    --love.graphics.line(xSCREEN(X1),ySCREEN(H1),xSCREEN(X1),ySCREEN(-H1),xSCREEN(X2),ySCREEN(-H2),xSCREEN(X2),ySCREEN(H2),xSCREEN(X1),ySCREEN(H1))
     for i=math.max(0,xSCREEN(X1)),math.min(xSCREEN(X2),SCREEN_W) do
         local new_h = (((((abs(i)-X1)/(X2-X1))*H2)+(((X2-abs(i))/(X2-X1))*H1))/2)*2
         if new_h >= h[i] then
@@ -104,6 +116,7 @@ function draw_segment(x1,y1,u1,x2,y2,u2,T)
         end
     end
 end
+
 xB=0
 yB=0
 
@@ -111,6 +124,7 @@ yB=0
 function love.draw()
     love.graphics.setBackgroundColor(1,1,1,1)
     love.graphics.draw(BACKGROUND)
+
     for i=0,800 do h[i]=0
     end
     for i=0,800 do texGlobal[i]=1
@@ -119,35 +133,17 @@ function love.draw()
     end
     for y = 1,L do  for x = 1,W do
         block = string.byte(level[y],x)
-        xB,yB=x*2-5,y*2-5
+        xB,yB=x*2,y*2
         --print(block)
-        if block == 49 then
+        if block >= 49 then
             xB1,yB1 = in_avatar_coord(xB,yB)
             xB2,yB2 = in_avatar_coord(xB+2,yB)
             xB3,yB3 = in_avatar_coord(xB+2,yB+2)
             xB4,yB4 = in_avatar_coord(xB,yB+2)
-            draw_segment(xB1,yB1,0,xB2,yB2,1,1)
-            draw_segment(xB2,yB2,0,xB3,yB3,1,1)
-            draw_segment(xB3,yB3,0,xB4,yB4,1,1)
-            draw_segment(xB4,yB4,0,xB1,yB1,1,1)
-        elseif block == 50 then
-            xB1,yB1 = in_avatar_coord(xB,yB)
-            xB2,yB2 = in_avatar_coord(xB+2,yB)
-            xB3,yB3 = in_avatar_coord(xB+2,yB+2)
-            xB4,yB4 = in_avatar_coord(xB,yB+2)
-            draw_segment(xB1,yB1,0,xB2,yB2,1,2)
-            draw_segment(xB2,yB2,0,xB3,yB3,1,2)
-            draw_segment(xB3,yB3,0,xB4,yB4,1,2)
-            draw_segment(xB4,yB4,0,xB1,yB1,1,2)
-        elseif block == 51 then
-            xB1,yB1 = in_avatar_coord(xB,yB)
-            xB2,yB2 = in_avatar_coord(xB+2,yB)
-            xB3,yB3 = in_avatar_coord(xB+2,yB+2)
-            xB4,yB4 = in_avatar_coord(xB,yB+2)
-            draw_segment(xB1,yB1,0,xB2,yB2,1,3)
-            draw_segment(xB2,yB2,0,xB3,yB3,1,3)
-            draw_segment(xB3,yB3,0,xB4,yB4,1,3)
-            draw_segment(xB4,yB4,0,xB1,yB1,1,3)
+            draw_segment(xB1,yB1,0,xB2,yB2,1,block-48)
+            draw_segment(xB2,yB2,0,xB3,yB3,1,block-48)
+            draw_segment(xB3,yB3,0,xB4,yB4,1,block-48)
+            draw_segment(xB4,yB4,0,xB1,yB1,1,block-48)
 
 
 
@@ -165,7 +161,11 @@ function love.draw()
                 love.graphics.draw(texture2,quad,i,ySCREEN(-h[i]),0,1,h[i])
             elseif texGlobal[i]==3 then
                 love.graphics.draw(texture3,quad,i,ySCREEN(-h[i]),0,1,h[i])
+            elseif texGlobal[i]==4 then
+                love.graphics.draw(texture4,quad,i,ySCREEN(-h[i]),0,1,h[i])
             end
+            
         end
     end
+    love.graphics.draw(UI)
 end
